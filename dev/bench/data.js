60c3ef1fba46bca0faec04254ab78e448a6a8d8f
github-action-benchmark <github@users.noreply.github.com>
2023-01-10 23:02:08 +0000

    add Benchmark result (noirblanc) (cargo) benchmark result for 7828e7f9111893c21ba33a0212609d3986156879

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1673391728399,
+  "repoUrl": "https://github.com/koba-e964/noirblanc",
+  "entries": {
+    "Benchmark result (noirblanc)": [
+      {
+        "commit": {
+          "author": {
+            "email": "3303362+koba-e964@users.noreply.github.com",
+            "name": "koba-e964",
+            "username": "koba-e964"
+          },
+          "committer": {
+            "email": "3303362+koba-e964@users.noreply.github.com",
+            "name": "koba-e964",
+            "username": "koba-e964"
+          },
+          "distinct": true,
+          "id": "7828e7f9111893c21ba33a0212609d3986156879",
+          "message": "Fix tests",
+          "timestamp": "2023-01-11T08:01:27+09:00",
+          "tree_id": "fcfc0422a3cb0887b039679d3e3b7f0dfb816aab",
+          "url": "https://github.com/koba-e964/noirblanc/commit/7828e7f9111893c21ba33a0212609d3986156879"
+        },
+        "date": 1673391727438,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "perft::tests::bench_perft_8",
+            "value": 3401603,
+            "range": "± 7224",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
